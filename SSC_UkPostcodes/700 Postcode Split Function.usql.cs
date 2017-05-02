b11db68c58347cb5c1258d67639d186d932e20a9
Mike McQuillan <mike.mcquillan@mcqtech.com>
2017-05-02 16:57:39 +0100

    Added code for stairway step 14 (local functions).

---
+++ SSC_UkPostcodes/700 Postcode Split Function.usql.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SSC_UkPostcodes
+{
+    public static class FunctionTests
+    {
+        public static string PostcodePart(string postcode, bool part1)
+        {
+            string pcode = null;
+
+            if (part1)
+            {
+                pcode = postcode.IndexOf(" ") == -1 ? postcode : postcode.Substring(0, postcode.IndexOf(" "));
+            }
+            else
+            {
+                pcode = postcode.IndexOf(" ") == -1 ? postcode : postcode.Substring(postcode.IndexOf(" ") + 1);
+            }
+
+            return pcode;
+        }
+    }
+}
