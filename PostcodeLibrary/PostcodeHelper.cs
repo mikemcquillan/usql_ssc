c3d6f0430ad449e83846e5d06b8132210670e1bd
Mike McQuillan <mike.mcquillan@mcqtech.com>
2017-08-15 14:20:54 +0100

    Added code for U-SQL stairway step 15 (global functions).

---
+++ PostcodeLibrary/PostcodeHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PostcodeLibrary
+{
+    public class PostcodeHelper
+    {
+        public static string PostcodePart1(string postcode)
+        {
+            return postcode.IndexOf(" ") == -1 ? postcode : postcode.Substring(0, postcode.IndexOf(" "));
+        }
+
+        public static string PostcodePart2(string postcode)
+        {
+            return postcode.IndexOf(" ") == -1 ? postcode : postcode.Substring(postcode.IndexOf(" ") + 1);
+        }
+    }
+}
